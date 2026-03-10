e1aa8e2329d69427972fb8e2786e00d7677fd011
Keneth <166479255+KenyIzConfused@users.noreply.github.com>
2026-03-10 22:49:39 +0800

    v 0.2

---
+++ scheduling-chart/scenes/inputs.gd
@@ -0,0 +1,7 @@
+extends Node2D
+@onready var employee_edit: LineEdit = $Control/employeeEdit
+@onready var tq_edit: LineEdit = $Control/tqEdit
+
+
+func _on_input_btn_pressed() -> void:
+	pass # Replace with function body.
